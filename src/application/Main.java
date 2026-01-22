1620a6b94c6b855b26f7107543e1e277f5e60372
Gaffaell <rafaelgiansiqueira@gmail.com>
2026-01-22 19:33:47 -0300

    First commit

---
+++ src/application/Main.java
@@ -0,0 +1,50 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    public static void main(String[] args) throws Exception {
+        launch(args); // Launches the JavaFX application
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Menu.fxml")); // Loads the FXML file
+        Scene scene = new Scene(root); // Creates a new scene with the loaded FXML
+        scene.getStylesheets().add(getClass().getResource("/style/Menu.css").toExternalForm()); // Adds the CSS stylesheet to the scene
+        
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            exitGame(stage);
+        });
+        
+        stage.setScene(scene); // Sets the scene on the stage
+        stage.setTitle("Brick Breaker Game");
+        stage.show(); // Displays the stage
+        
+    }
+
+    public void exitGame(Stage stage) {
+        
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Exit Game");
+        alert.setHeaderText("You are about to exit the game.");
+        alert.setContentText("Are you sure you want to exit?");
+
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+
+            stage = (Stage) stage.getScene().getWindow(); // Gets the current stage
+            stage.close(); // Closes the stage
+
+        }
+
+    }
+}
---
+++ src/application/ScoreController.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class ScoreController {
+    
+    @FXML
+    private Button menuButton;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToMenuScene(ActionEvent event) throws IOException {
+        
+        root = FXMLLoader.load(getClass().getResource("/fxml/Menu.fxml")); // Loads the Game FXML file
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow(); // Gets the current stage
+        scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/style/Menu.css").toExternalForm()); // Adds the CSS stylesheet to the scene
+        stage.setScene(scene); // Sets the new scene on the stage
+        stage.show();
+
+    }
+
+}
