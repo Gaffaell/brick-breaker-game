632a36957fa792afe3935af8280626ba06d4b7f7
Rafael Siqueira <rafaelgiansiqueira@gmail.com>
2026-01-22 21:11:06 -0300

    feat: implementation of the ball and a initial animation

--- src/application/GameController.java
+++ src/application/GameController.java
@@ -1,17 +1,23 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
-public class GameController {
+public class GameController implements Initializable{ // Implements Initializable for game setup
 
     @FXML
     private Button menuButton;
@@ -29,5 +35,24 @@ public class GameController {
         stage.show();
 
     }
+
+    @FXML
+    private Circle ball; // Example of a game element
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+        TranslateTransition translate = new TranslateTransition();
+        translate.setNode(ball);
+        translate.setDuration(Duration.millis(1000));
+        translate.setCycleCount(TranslateTransition.INDEFINITE);
+        translate.setByY(-300);
+        translate.setAutoReverse(true); // if the ball hits something
+        translate.play();
+
+        
+    }
+
+
 
 }
