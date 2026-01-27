728694ef843f6cfe8c5b26728060af28eccc3da2
Rafael Siqueira <rafaelgiansiqueira@gmail.com>
2026-01-27 10:18:38 -0300

    feat:gameloop and inicial ball animation

--- src/application/GameController.java
+++ src/application/GameController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.AnimationTimer;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,15 +16,30 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
-public class GameController implements Initializable{ // Implements Initializable for game setup
+public class GameController extends AnimationTimer implements Initializable { // Implements Initializable, loads when the scene is created
 
     @FXML
     private Button menuButton;
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    private Circle ball;
+    private TranslateTransition circleAnimation = new TranslateTransition();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Create and start the animation when the scene loads
+        circleAnimation.setNode(ball);
+        circleAnimation.setByY(-300);
+        circleAnimation.setCycleCount(TranslateTransition.INDEFINITE);
+        circleAnimation.setAutoReverse(true);
+        circleAnimation.play();
+        
+        // Start the animation timer
+        this.start();
+    }
 
     public void switchToMenuScene(ActionEvent event) throws IOException {
         
@@ -36,23 +52,19 @@ public class GameController implements Initializable{ // Implements Initializabl
 
     }
 
-    @FXML
-    private Circle ball; // Example of a game element
-
     @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
+    public void handle(long now) {
+        
+        doHandle();
+        
+    }
 
-        TranslateTransition translate = new TranslateTransition();
-        translate.setNode(ball);
-        translate.setDuration(Duration.millis(1000));
-        translate.setCycleCount(TranslateTransition.INDEFINITE);
-        translate.setByY(-300);
-        translate.setAutoReverse(true); // if the ball hits something
-        translate.play();
+    private void doHandle() {
+        // Game loop logic goes here
 
         
     }
 
 
-
 }
+
