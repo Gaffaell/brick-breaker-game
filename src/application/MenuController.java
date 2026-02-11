f5b78d28fad6acd53f1e6f30f6a9f707649f68c1
Rafael Siqueira <rafaelgiansiqueira@gmail.com>
2026-02-11 17:52:25 -0300

    feat/ brick are disappearing when hit/ implementation of the list of bricks

--- src/application/MenuController.java
+++ src/application/MenuController.java
@@ -16,54 +16,58 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class MenuController {
-    
-    @FXML
-    private AnchorPane menuPane;
-    @FXML
-    private Button startGame;
-    @FXML
-    private Button exitGame;
-    @FXML
-    private Button scoreGame;
-    
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
 
-    public void switchToGameScene(ActionEvent event) throws IOException {
-        
-        root = FXMLLoader.load(getClass().getResource("/fxml/Game.fxml")); // Loads the Game FXML file
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow(); // Gets the current stage
-        scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("/style/Game.css").toExternalForm()); // Adds the CSS stylesheet to the scene
-        stage.setScene(scene); // Sets the new scene on the stage
-        stage.show();
+  @FXML
+  private AnchorPane menuPane;
+  @FXML
+  private Button startGame;
+  @FXML
+  private Button exitGame;
+  @FXML
+  private Button scoreGame;
+
+  private Stage stage;
+  private Scene scene;
+  private Parent root;
+
+  public void switchToGameScene(ActionEvent event) throws IOException {
+
+    root = FXMLLoader.load(getClass().getResource("/fxml/Game.fxml")); // Loads the Game FXML file
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); // Gets the current stage
+    scene = new Scene(root);
+    scene.getStylesheets().add(getClass().getResource("/style/Game.css").toExternalForm()); // Adds the CSS stylesheet
+                                                                                            // to the scene
+    stage.setScene(scene); // Sets the new scene on the stage
+    stage.show();
+
+  }
+
+  public void switchToScoreScene(ActionEvent event) throws IOException {
+
+    root = FXMLLoader.load(getClass().getResource("/fxml/Score.fxml")); // Loads the Game FXML file
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); // Gets the current stage
+    scene = new Scene(root);
+    scene.getStylesheets().add(getClass().getResource("/style/Score.css").toExternalForm()); // Adds the CSS stylesheet
+                                                                                             // to the scene
+    stage.setScene(scene); // Sets the new scene on the stage
+    stage.show();
+
+  }
+
+  public void exitGame(ActionEvent event) throws IOException {
+
+    Alert alert = new Alert(AlertType.CONFIRMATION);
+    alert.setTitle("Exit Game");
+    alert.setHeaderText("You are about to exit the game.");
+    alert.setContentText("Are you sure you want to exit?");
+
+    if (alert.showAndWait().get() == ButtonType.OK) {
+
+      stage = (Stage) menuPane.getScene().getWindow(); // Gets the current stage
+      stage.close(); // Closes the stage
 
     }
-    public void switchToScoreScene(ActionEvent event) throws IOException {
-        
-        root = FXMLLoader.load(getClass().getResource("/fxml/Score.fxml")); // Loads the Game FXML file
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow(); // Gets the current stage
-        scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("/style/Score.css").toExternalForm()); // Adds the CSS stylesheet to the scene
-        stage.setScene(scene); // Sets the new scene on the stage
-        stage.show();
 
-    }
-    public void exitGame(ActionEvent event) throws IOException {
-        
-        Alert alert = new Alert(AlertType.CONFIRMATION);
-        alert.setTitle("Exit Game");
-        alert.setHeaderText("You are about to exit the game.");
-        alert.setContentText("Are you sure you want to exit?");
-
-        if (alert.showAndWait().get() == ButtonType.OK) {
-
-            stage = (Stage) menuPane.getScene().getWindow(); // Gets the current stage
-            stage.close(); // Closes the stage
-
-        }
-
-    }
+  }
 
 }
--- src/application/ScoreController.java
+++ src/application/ScoreController.java
@@ -12,22 +12,23 @@ import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class ScoreController {
-    
-    @FXML
-    private Button menuButton;
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
 
-    public void switchToMenuScene(ActionEvent event) throws IOException {
-        
-        root = FXMLLoader.load(getClass().getResource("/fxml/Menu.fxml")); // Loads the Game FXML file
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow(); // Gets the current stage
-        scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("/style/Menu.css").toExternalForm()); // Adds the CSS stylesheet to the scene
-        stage.setScene(scene); // Sets the new scene on the stage
-        stage.show();
+  @FXML
+  private Button menuButton;
+  private Stage stage;
+  private Scene scene;
+  private Parent root;
 
-    }
+  public void switchToMenuScene(ActionEvent event) throws IOException {
+
+    root = FXMLLoader.load(getClass().getResource("/fxml/Menu.fxml")); // Loads the Game FXML file
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); // Gets the current stage
+    scene = new Scene(root);
+    scene.getStylesheets().add(getClass().getResource("/style/Menu.css").toExternalForm()); // Adds the CSS stylesheet
+                                                                                            // to the scene
+    stage.setScene(scene); // Sets the new scene on the stage
+    stage.show();
+
+  }
 
 }
